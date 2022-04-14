c88cedb3693aafcd95d264220c9bff2508cd9c99
nicolehargrove <nicole.hargrove@strategio.tech>
2022-04-14 16:28:38 -0400

    committing first time hello World 2

---
+++ tech.strategio.helloworld2/src/tech/strategio/helloworld2/HelloWorld2Eclipse2.java
@@ -0,0 +1,11 @@
+package tech.strategio.helloworld2;
+
+public class HelloWorld2Eclipse2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World Demo 2 With the Team");
+
+	}
+
+}
